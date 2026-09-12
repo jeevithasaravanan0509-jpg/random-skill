1d04b2b1971b42fa4dd73e16e970d832ae4b845b
Jeevitha Saravanan <jeevithasaravanan0509@gmail.com>
2026-09-12 08:53:46 +0530

    Initial Random Skill project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Random Skill</h1>
+      <p>Turn your free time into a useful skill.</p>
+
+      <button>Surprise Me</button>
+    </div>
+  )
+}
+
+export default App
